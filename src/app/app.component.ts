df3f60caefa181db07e103f000d65ae455fb80d4
paulav197 <paulaalcazarrubio@hotmail.es>
2024-11-21 08:36:39 +0100

    He dividido la pagina en componentes y en body he intentado crear un tablero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { BodyComponentComponent } from './body-component/body-component.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponentComponent, BodyComponentComponent, FooterComponentComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
